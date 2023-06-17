2b93593deff911cedc011f36de72d892473d52d4
dautmazlami <daut.mazlami@gmail.com>
2023-06-17 22:07:04 +0200

    minor changes

--- index.js
+++ index.js
@@ -1,5 +1,63 @@
 // Fetch the JSON data from the local file
 
+// fetch("data.json")
+//   .then(function(response) {
+//     if (!response.ok) {
+//       throw new Error("Error fetching JSON data");
+//     }
+//     return response.json();
+//   })
+//   .then(function(data) {
+//     var feedContainer = document.querySelector(".layout-container");
+//     var loadMoreBtn = document.querySelector(".load-more-btn");
+//     const lightbox = document.getElementById('lightbox');
+//     const lightboxImage = document.querySelector('.lightbox-image');
+//     const closeBtn = document.querySelector('.close-btn');
+
+
+//     var visiblePosts = 4;
+    
+//     function showPosts() {
+//       for (var i = 0; i < visiblePosts; i++) {
+//         if (i >= data.length) {
+//           loadMoreBtn.style.display = "none";
+//           break;
+//         }
+
+//         var post = data[i];
+//         var postCard = document.createElement("div");
+//         postCard.classList.add("post-card");
+
+//         var postContent = "";
+//         if (post.type === "image") {
+//           postContent += '<img src="' + post.image + '" alt="Post Image">';
+//         }
+//         postContent += '<div class="card-content">' +
+//                           '<h4>' + post.caption + '</h4>' +
+//                           '<p>' + post.likes + ' Likes</p>' +
+//                           '<p>Source: ' + post.source_type + '</p>' +
+//                           '<div class="user-info">' +
+//                             '<img src="' + post.profile_image + '" alt="Profile Image" class="profile-image">' +
+//                             '<span class="user-name">' + post.name + '</span>' +
+//                           '</div>' +
+//                         '</div>';
+//         postCard.innerHTML = postContent;
+
+//         feedContainer.appendChild(postCard);
+//       }
+//     }
+
+//     showPosts();
+
+//     loadMoreBtn.addEventListener("click", function() {
+//       visiblePosts += 4;
+//       showPosts();
+//     });
+//   })
+//   .catch(function(error) {
+//     console.error(error.message);
+//   });
+
 fetch("data.json")
   .then(function(response) {
     if (!response.ok) {
@@ -10,9 +68,12 @@ fetch("data.json")
   .then(function(data) {
     var feedContainer = document.querySelector(".layout-container");
     var loadMoreBtn = document.querySelector(".load-more-btn");
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImage = document.querySelector('.lightbox-image');
+    const closeBtn = document.querySelector('.close-btn');
 
     var visiblePosts = 4;
-    
+
     function showPosts() {
       for (var i = 0; i < visiblePosts; i++) {
         if (i >= data.length) {
@@ -24,20 +85,44 @@ fetch("data.json")
         var postCard = document.createElement("div");
         postCard.classList.add("post-card");
 
-        var postContent = "";
+        var profileImage = document.createElement("img");
+        profileImage.src = post.profile_image;
+        profileImage.alt = "Profile Image";
+        profileImage.classList.add("profile-image");
+        postCard.appendChild(profileImage);
+
+        var userName = document.createElement("span");
+        userName.classList.add("user-name");
+        userName.textContent = post.name;
+        postCard.appendChild(userName);
+
+        var cardContent = document.createElement("div");
+        cardContent.classList.add("card-content");
+
+        var caption = document.createElement("h4");
+        caption.textContent = post.caption;
+
         if (post.type === "image") {
-          postContent += '<img src="' + post.image + '" alt="Post Image">';
+          var postImage = document.createElement("img");
+          postImage.src = post.image;
+          postImage.alt = "Post Image";
+          postImage.addEventListener("click", function() {
+            lightbox.style.display = "block";
+            lightboxImage.src = post.image;
+          });
+          postCard.appendChild(postImage);
         }
-        postContent += '<div class="card-content">' +
-                          '<h4>' + post.caption + '</h4>' +
-                          '<p>' + post.likes + ' Likes</p>' +
-                          '<p>Source: ' + post.source_type + '</p>' +
-                          '<div class="user-info">' +
-                            '<img src="' + post.profile_image + '" alt="Profile Image" class="profile-image">' +
-                            '<span class="user-name">' + post.name + '</span>' +
-                          '</div>' +
-                        '</div>';
-        postCard.innerHTML = postContent;
+
+        var likes = document.createElement("p");
+        likes.textContent = post.likes + " Likes";
+
+        var source = document.createElement("p");
+        source.textContent = "Source: " + post.source_type;
+
+        cardContent.appendChild(caption);
+        cardContent.appendChild(likes);
+        cardContent.appendChild(source);
+        postCard.appendChild(cardContent);
 
         feedContainer.appendChild(postCard);
       }
@@ -49,10 +134,15 @@ fetch("data.json")
       visiblePosts += 4;
       showPosts();
     });
+
+    closeBtn.addEventListener("click", function() {
+      lightbox.style.display = "none";
+    });
   })
   .catch(function(error) {
     console.error(error.message);
   });
+
 
 // render the social media feed
 function renderSocialMediaFeed(data) {
@@ -65,17 +155,17 @@ function renderSocialMediaFeed(data) {
     const card = document.createElement('div');
     card.classList.add('post-card');
 
+    const profileImage = document.createElement('img');
+    profileImage.src = post.profile_image;
+    profileImage.classList.add('profile-image');
+    cardContent.appendChild(profileImage);
+
     const image = document.createElement('img');
     image.src = post.image;
     card.appendChild(image);
 
     const cardContent = document.createElement('div');
     cardContent.classList.add('card-content');
-
-    const profileImage = document.createElement('img');
-    profileImage.src = post.profile_image;
-    profileImage.classList.add('profile-image');
-    cardContent.appendChild(profileImage);
 
     const name = document.createElement('h4');
     name.textContent = post.name;
@@ -106,7 +196,6 @@ function renderSocialMediaFeed(data) {
 
   // Render the initial batch of posts
   renderPosts(0, postsPerPage);
-
 
 // switch between dark and light theme
   document.addEventListener('click', function () {
